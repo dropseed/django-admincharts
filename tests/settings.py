14b312a5672042c790dbde01dd717d8ed64874b4
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-07-05 11:22:31 -0500

    Set explicit test key for test SECRET_KEY

--- tests/settings.py
+++ tests/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=p%&d82bn%pzy4s*9@052$x!%2+d9-5m-8f6+o**)a)=jtdfeh"
+SECRET_KEY = "test"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
